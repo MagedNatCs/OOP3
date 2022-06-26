7f9e79ca37666872b0152f1b895cab9e1271eb4f
AmeenKassem <kassemmo@post.bgu.ac.il>
2022-06-26 14:10:18 +0300

    Added fields for UNIT with setters and getters

--- OOPhw3/src/BusinessLayer/Tiles/Unit.java
+++ OOPhw3/src/BusinessLayer/Tiles/Unit.java
@@ -3,9 +3,46 @@ package BusinessLayer.Tiles;
 
 public abstract class Unit extends Tile {
 
-    private string name;
+    private String name;
+    private int health;
+    private int attack;
+    private int defense;
+
+
 
     public String describe() {
         return String.format("%s\t\tHealth: %s\t\tAttack: %d\t\tDefense: %d", getName(), getHealth(), getAttack(), getDefense());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
     }
 }
